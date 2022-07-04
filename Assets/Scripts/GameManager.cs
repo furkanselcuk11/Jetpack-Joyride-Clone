a961fbb20d73aa68e4835315b6d1095294d23deb
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2022-07-04 21:18:48 +0300

    +add: Parkour, Player and Baiers added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gamemanagerInstance;
+
+    private void Awake()
+    {
+        if (gamemanagerInstance == null)
+        {
+            gamemanagerInstance = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
